1cc28e88309334f4fb4920b0ff638c6fc04b3aca
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-07 10:53:44 +0200

    8 kyu Grasshopper - Summation

---
+++ 8kyu/8 kyu Grasshopper - Summation/8 kyu Grasshopper - Summation.ts
@@ -0,0 +1,19 @@
+/**
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+ */
+export const summation = (num:number)=> { 
+    // Code here
+    let sum:number = 0;
+    for(let i =0; i<=num; i++){
+      sum+= i
+    }
+    return sum
+  }
+  console.log(summation(5))
+  
