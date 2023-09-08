f751984c6e8c8ba2f5a0d9f6cd641620f0d3a873
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-08 20:47:05 +0200

    Moving Zeros To The End

---
+++ 5kyu/Moving Zeros To The End/Moving Zeros To The End.js
@@ -0,0 +1,11 @@
+/**
+ DESCRIPTION:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+ */
+
+const moveZeros = (arr) => [
+  ...arr.filter((el) => el !== 0),
+  ...arr.filter((el) => el === 0),
+];
