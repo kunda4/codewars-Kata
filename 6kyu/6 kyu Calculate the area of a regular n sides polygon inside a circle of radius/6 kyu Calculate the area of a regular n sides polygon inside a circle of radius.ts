c7f83360eaa9128aebcf31ae29506ae43b3bf337
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-08 13:01:44 +0200

    6 kyu Calculate the area of a regular n sides polygon inside a circle of radius

---
+++ 6kyu/6 kyu Calculate the area of a regular n sides polygon inside a circle of radius/6 kyu Calculate the area of a regular n sides polygon inside a circle of radius.ts
@@ -0,0 +1,22 @@
+/**
+Write the following function:
+
+function areaOfPolygonInsideCircle(circleRadius, numberOfSides)
+It should calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle).
+
+The answer should be a number rounded to 3 decimal places.
+
+Input :: Output Examples
+
+areaOfPolygonInsideCircle(3, 3) // returns 11.691
+
+areaOfPolygonInsideCircle(5.8, 7) // returns 92.053
+
+areaOfPolygonInsideCircle(4, 5) // returns 38.042
+Note: if you need to use Pi in your code, use the native value of your language unless stated otherwise.
+ */
+
+export function areaOfPolygonInsideCircle(r: number, n: number): number {
+    const area = (n / 2) * r * r * Math.sin((360 / n * Math.PI) / 180);
+   return parseFloat(area.toFixed(3));
+ }
