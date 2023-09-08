63154dea1a272d29d16c443230e8342ed3d0c2be
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-08 21:00:21 +0200

    3 kyu The soul of wit: reverse an array

---
+++ 3kyu/The soul of wit: reverse an array/3 kyu The soul of wit: reverse an array.js
@@ -0,0 +1,14 @@
+/**
+ DESCRIPTION:
+No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use Array.prototype.reverse.
+
+You have 47 bytes to spare.
+
+Example: [1, 2, 3] → [3, 2, 1]
+
+And this time you won't be able to do the thing from that kata.
+
+require, import and from are't allowed as well.
+ */
+
+reverse = (a) => [...a].map(a.pop, a);
