7518d430c44e336cb83c85011ba2c8a24ab337eb
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-09 23:49:58 +0200

    8 kyu Sum Mixed Array

---
+++ 8kyu/8 kyu Sum Mixed Array/8 kyu Sum Mixed Array.js
@@ -0,0 +1,9 @@
+/**
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+ */
+function sumMix(x) {
+  return x.map(Number).reduce((acc, curr) => acc + curr, 0);
+}
