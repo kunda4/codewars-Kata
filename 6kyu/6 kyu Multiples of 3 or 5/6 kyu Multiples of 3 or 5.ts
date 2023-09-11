caf5a746c5b284c0a56e7766afbe544b67fe3e82
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-11 22:03:40 +0200

    6 kyu Multiples of 3 or 5

---
+++ 6kyu/6 kyu Multiples of 3 or 5/6 kyu Multiples of 3 or 5.ts
@@ -0,0 +1,26 @@
+/**
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+
+export class Challenge {
+    static solution(num: number){
+      let myArr =[]
+    for (let i=1; i<num; i++){
+      if(i%3===0 && i%5===0){
+        myArr.push(i)
+      }else if(i%3===0){
+        myArr.push(i)
+      
+    }else if(i%5===0){
+        myArr.push(i)
+      }
+      }
+  return myArr.reduce((acc,curr)=> acc+curr,0)
+    }
+  }
