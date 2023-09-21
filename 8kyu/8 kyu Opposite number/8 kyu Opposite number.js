9fa70cd09b566d8dfa3c72ac7253abc748b26e5c
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-21 22:44:20 +0200

    8 kyu Opposite number

---
+++ 8kyu/8 kyu Opposite number/8 kyu Opposite number.js
@@ -0,0 +1,13 @@
+/**
+/**
+ DESCRIPTION:
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+ */
+
+const opposite = (n) => n * -1;
---
+++ 8kyu/8 kyu Opposite number/8 kyu Opposite number.ts
@@ -0,0 +1,16 @@
+/**
+ DESCRIPTION:
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+ */
+
+export class Kata {
+    static opposite(n: number) {
+      return n*-1
+    }
+  }
