3ebdabd5d45e8bcf392f6e139b1fbf1b13d8172a
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-23 20:46:23 +0200

    8 kyu Training JS #14: Methods of Number object--toString() and toLocaleString()

--- 6kyu/6 kyu Detect Pangram/6 kyu Detect Pangram.ts
+++ 6kyu/6 kyu Detect Pangram/6 kyu Detect Pangram.ts
@@ -4,7 +4,6 @@ A pangram is a sentence that contains every single letter of the alphabet at lea
 
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
-
  */
 
 export function isPangram(phrase: string): boolean{
