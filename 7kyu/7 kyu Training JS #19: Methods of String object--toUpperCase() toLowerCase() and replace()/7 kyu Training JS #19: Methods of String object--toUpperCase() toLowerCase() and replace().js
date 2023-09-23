4007c1120700ee81895183c019e82877c6d97c2a
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-23 20:32:50 +0200

    7 kyu Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()

---
+++ 7kyu/7 kyu Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()/7 kyu Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js
@@ -0,0 +1,21 @@
+/**
+Task
+Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+
+We translate the sentence into an alien language according to the following rules:
+
+Each word in the sentence is separated by spaces. The last letter of each word in the sentence turns to lowercase, and the other letters are capitalized. Looks very strange? Because this is the form of alien language ;-)
+
+for example:
+
+alienLanguage("My name is John") should return "My NAMe Is JOHn"
+alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
+alienLanguage("Hello World") should return "HELLo WORLd"
+A small hint: The first conversion of the entire string will make the code easier
+ */
+
+const alienLanguage = (str) =>
+  str
+    .split(" ")
+    .map((el) => el.slice(0, -1).toUpperCase() + el.slice(-1).toLowerCase())
+    .join(" ");
