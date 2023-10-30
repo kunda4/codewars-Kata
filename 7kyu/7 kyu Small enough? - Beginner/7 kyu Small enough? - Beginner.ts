2134ae34ae0ad540c354508e511621d3c5c21677
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-30 20:41:03 +0200

    7 kyu Small enough? - Beginner

---
+++ 7kyu/7 kyu Small enough? - Beginner/7 kyu Small enough? - Beginner.ts
@@ -0,0 +1,10 @@
+/**
+ DESCRIPTION:
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+ */
+
+export function smallEnough(a: number[], limit: number): boolean{
+    return a.every((el)=> el<=limit)
+  }
